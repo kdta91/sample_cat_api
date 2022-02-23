89b18425c7452efd05f8b90d1d6d89794062c2b6
kdta91 <kristoff@mavrlab.com>
2022-02-23 18:08:03 +0800

    Revert "Updated readme file"
    This reverts commit c9b96c9acbc29f712994e6f21102617198028824.

--- src/components/AlertNotification.jsx
+++
@@ -1,7 +0,0 @@
-import Alert from "react-bootstrap/Alert";
-
-const AlertNotification = ({ variant, text }) => {
-  return <Alert variant={variant}>{text}</Alert>;
-};
-
-export default AlertNotification;
